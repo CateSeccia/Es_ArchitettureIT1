5d4fadaf03b6f83e775cb73fe953ff67fc40c916
Seccia Caterina <caterina.seccia@edu-its.it>
2026-06-09 16:47:21 +0200

    modifica minima in salva

--- salva_ip.php
+++ salva_ip.php
@@ -38,6 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             //echo "IP salvato con successo!";
             // Puoi anche reindirizzare l'utente dopo il successo:
             header("Location: index.php");
+            exit();
         } else {
             echo "Si è verificato un errore durante il salvataggio.";
         }
